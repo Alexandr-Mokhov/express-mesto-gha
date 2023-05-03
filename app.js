b6c120c7c307f8fdfd25638317993b4eb4baf400
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-05-03 16:30:29 +0500

    feat: создать схемы и модели

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 // const path = require('path');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/mestodb');
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => console.log('test'));
